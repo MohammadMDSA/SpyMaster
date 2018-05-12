62996c9365b30bea32612f24550807bbd2d71c7b
Mohammad MDSA <samimd.77@gmail.com>
2018-05-12 14:12:06 +0430

    Changed password box

--- SpyMaster/Pages/LoginPage.xaml.cs
+++ SpyMaster/Pages/LoginPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using InstaSharper.API;
+using InstaSharper.API.Builder;
+using InstaSharper.Classes;
+using InstaSharper.Logger;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -27,9 +31,86 @@ namespace SpyMaster.Pages
 			this.InitializeComponent();
 		}
 
-		private void LoginBtn_Click(object sender, RoutedEventArgs e)
+		private async void LoginBtn_Click(object sender, RoutedEventArgs e)
 		{
-			(Window.Current.Content as Frame).Navigate(typeof(MainPage));
+			try
+			{
+				Console.WriteLine("Starting demo of InstaSharper project");
+				// create user session data and provide login details
+				var userSession = new UserSessionData
+				{
+					//UserName = "The_Samimd",
+					UserName = UsernameBox.Text,
+					//Password = "The2nd"
+					Password = PasswordBox.Password
+				};
+
+				var delay = TimeSpan.FromSeconds(2);
+				// create new InstaApi instance using Builder
+				MainPage.InstaApi = InstaApiBuilder.CreateBuilder()
+					.SetUser(userSession)
+					.UseLogger(new DebugLogger(LogLevel.Exceptions)) // use logger for requests and debug messages
+					.SetRequestDelay(delay)
+					.Build();
+				//// create account
+				//var username = "kajokoleha";
+				//var password = "ramtinjokar";
+				//var email = "ramtinak@live.com";
+				//var firstName = "Ramtin";
+				//var accountCreation = await _instaApi.CreateNewAccount(username, password, email, firstName);
+
+				//const string stateFile = "state.bin";
+				//try
+				//{
+				//	if (File.Exists(stateFile))
+				//	{
+				//		Console.WriteLine("Loading state from file");
+				//		using (var fs = File.OpenRead(stateFile))
+				//		{
+				//			_instaApi.LoadStateDataFromStream(fs);
+				//		}
+				//	}
+				//}
+				//catch (Exception eev)
+				//{
+				//	Console.WriteLine(eev);
+				//}
+
+				if (!MainPage.InstaApi.IsUserAuthenticated)
+				{
+					// login
+					Console.WriteLine($"Logging in as {userSession.UserName}");
+					//delay.Disable();
+					var logInResult = await MainPage.InstaApi.LoginAsync();
+					//delay.Enable();
+					if (!logInResult.Succeeded)
+					{
+						Console.WriteLine($"Unable to login: {logInResult.Info.Message}");
+						//return false;
+						return;
+					}
+
+					(Window.Current.Content as Frame).Navigate(typeof(MainPage));
+					//var state = _instaApi.GetStateDataAsStream();
+					//using (var fileStream = File.Create(stateFile))
+					//{
+					//	state.Seek(0, SeekOrigin.Begin);
+					//	state.CopyTo(fileStream);
+					//}
+					
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+			}
+			finally
+			{
+				//	// perform that if user needs to logged out
+				//	// var logoutResult = Task.Run(() => _instaApi.LogoutAsync()).GetAwaiter().GetResult();
+				//	// if (logoutResult.Succeeded) Console.WriteLine("Logout succeed");
+			}
+			//return false;
 		}
 	}
 }
